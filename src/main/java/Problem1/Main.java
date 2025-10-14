1c403d290926efee2dd9130bb175d016e47f9beb
saif_eldeen <es.seifeldin.shady2023@alexu.edu.eg>
2025-10-14 23:05:53 +0300

    fixed some errors

--- src/main/java/Problem1/Main.java
+++ src/main/java/Problem1/Main.java
@@ -3,6 +3,9 @@ package Problem1;
 public class Main {
     public static void main(String[] args) {
         Person p = new Person("saif", 20000, true, 1000);
-        p.
+        System.out.println("name is "+p.getName());
+        System.out.println("salary is "+p.getSalary());
+        System.out.println("is working "+p.getIsWorking());
+        System.out.println("monthly expenses "+p.getMonthlyExpenses());
     }
 }
