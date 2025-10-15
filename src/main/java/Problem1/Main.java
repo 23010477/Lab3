e9a88ac4a00dde60d204e85612a054c29682b827
saif_eldeen <es.seifeldin.shady2023@alexu.edu.eg>
2025-10-15 16:42:44 +0300

    final touches

--- src/main/java/Problem1/Main.java
+++ src/main/java/Problem1/Main.java
@@ -2,7 +2,10 @@ package Problem1;
 
 public class Main {
     public static void main(String[] args) {
-        Person p = new Person("saif", 20000, true, 1000);
+        Person p = new Person("saif");
+        p.setMonthlyExpenses(0);
+        p.setIsWorking(false);
+        p.setSalary(1000);
         System.out.println("name is "+p.getName());
         System.out.println("salary is "+p.getSalary());
         System.out.println("is working "+p.getIsWorking());
